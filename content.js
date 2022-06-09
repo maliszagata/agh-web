ee7449d7be14f0b7c88f2fa8aa98325617e47589
agata_m <agata.malisz2@gmail.com>
2022-06-09 21:30:42 +0200

    first commit

---
+++ content.js
@@ -0,0 +1,41 @@
+const roslinyZieloneText = `Rośliny pokojowe, rośliny doniczkowe – rośliny ozdobne uprawiane w różnego rodzaju naczyniach w pomieszczeniach zamkniętych (np. w mieszkaniach, biurach, szklarniach). Służą do ozdoby pomieszczeń i umożliwiają w nich kontakt z naturą. Zazwyczaj są to rośliny wieloletnie, często byliny i pnącza, nierzadko też gatunki, które w naturze są drzewami. Uprawa w doniczkach zminiaturyzowanych drzew zwana jest techniką bonsai. Przedmiotem upraw są rośliny wyróżniające się walorami estetycznymi – o oryginalnym pokroju, efektowne ulistnione lub kwitnące.`;
+
+
+let roslinyZielone = {
+    title: "Rośliny zielone",
+    content: roslinyZieloneText,
+    images: `<img src="images/roslinyZielone.jpg" alt="rosliny">`
+}
+
+
+const roslinyKwitnaceText = `Kwiaty doniczkowe kwitnące cały rok to najczęściej rośliny o trwałych kwiatach lub o nieprzerwanym, powtarzającym się kwitnieniu. Ciągle kolorowe i atrakcyjne ożywią każde wnętrze. Jeśli mamy miejsce tylko dla jednej kwitnącej rośliny lub chcemy wprowadzić kolorową plamę w bezbarwne otoczenie, kwiaty doniczkowe kwitnące cały rok będą idealnie spełniały te zadania. Zazwyczaj takie rośliny będą lepiej kwitły, jeśli regularnie będziemy usuwać ich przekwitłe kwiatostany oraz zapewnimy im odpowiednie warunki uprawy.`;
+
+
+let roslinyKwitnace = {
+    title: "Rośliny kwitnące",
+    content: roslinyKwitnaceText,
+    images: `<img src="images/roslinyKwitnace.jpg" alt="rosliny">`
+}
+
+const pielegnacjaText = `Pielęgnacja roślin doniczkowych w zależności od pory roku
+Niektóre zasady dotyczące pór roku, które warto znać, odnoszą się do wszystkich roślin. Zastosowanie się do nich pozwoli Ci uniknąć wielu nieprzyjemności, a nawet być może uratuje kilka sadzonek przed złymi decyzjami.
+<br><br>
+<b>Zima – wiosna</b>
+<br><br>
+Pielęgnacja roślin pokojowych zimą i wczesną wiosną nie należy do najprostszych. Często wtedy łodygi wydłużają się w poszukiwaniu lepszego oświetlenia, rozwijają się coraz mniejsze listki i mało który śmiałek ma siłę kwitnąć. Oczywiście istnieją rośliny, które mają odwagę rozwijać pąki kwiatowe w zimowych miesiącach.
+Niestety większość, a nawet niemalże wszystkie kwiaty do domu nie są wtedy w najlepszej kondycji i prezentują się dość nieestetycznie. Jak o nie zadbać? Najważniejsze, by w tym okresie ich nie nawozić (lub nawozić specjalnymi „zimowymi nawozami”) oraz zapewniać możliwie największą ilość promieni słonecznych lub oświetlenie z lamp. Ważne jest również zadbanie o odpowiednią wilgotność powietrza, która zwykle jest niska przez to, że mamy włączone kaloryfery skutecznie wysuszające powietrze
+Pamiętaj również o temperaturach – otwarte okno bez zabrania roślin z parapetu zimą lub spacer mroźną porą do domu z roślinką pod pachą może skończyć się katastrofą. Większość roślin domowych źle reaguje na temperatury niższe niż 17 stopni Celsjusza!
+Gdy wiosna rozpocznie się na dobre, możesz powrócić do nawożenia swoich zielonych pociech, zakręcić kaloryfery i zacząć wyczekiwać pierwszych oznak wprowadzenia lepszej rutyny pielęgnacyjnej. Kwiaty domowe kwitnące już w tym momencie mogą zacząć swoje popisy ślicznymi, kolorowymi płatkami, a uśpione bulwy ruszą raz-dwa!
+<br><br>
+<b>Lato – jesień</b>
+<br><br>
+Pora letnia to czas, gdy kwiaty doniczkowe domowe czują się bosko. Koniecznie podlewaj wtedy je regularnie, nie zapominając o nawożeniu. Jeżeli masz taką możliwość – możesz nawet wystawić je na balkon, tam będzie im jeszcze lepiej.
+Oczywiście lato ma też swoje minusy – jednym z nich jest pojawienie się większego zagrożenia szkodnikami roślin doniczkowych. Mogą one się pojawić w Twoim mieszkaniu przyniesione wraz z nowym roślinnym nabytkiem, na owocach lub warzywach, a nawet na ubraniu! Równie dobrze mogą dostać się do niego przez uchylone okno albo otwarty balkon.
+Jesienią uważaj na ilość wlewanej wody do doniczek i powoli zacznij wprowadzać „zimowy nawóz”, a także stopniowo zaprzestawaj używania standardowego. Rozpocznij przygotowania roślin jednorocznych na zimowy spoczynek – odpowiednio uśpione ruszą już z pierwszymi dniami wiosny!`;
+
+
+let pielegnacja = {
+    title: "Pielęgnacja roślin domowych",
+    content: pielegnacjaText,
+    images: `<img src="images/pielegnacja.jpeg" alt="rosliny">`
+}
